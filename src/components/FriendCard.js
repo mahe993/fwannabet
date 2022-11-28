ad1333bff57e27b67ac5f58924489bd674c49053
Zihao1319 <zihao1319@gmail.com>
2022-11-28 23:28:35 +0800

    basic styling for friend card

--- src/components/FriendCard.js
+++ src/components/FriendCard.js
@@ -1,5 +1,6 @@
 import { Box } from "@mui/material";
 import React from "react";
+import { Button } from "@mui/material";
 
 const FriendCard = (props) => {
   const { user } = props;
@@ -7,9 +8,33 @@ const FriendCard = (props) => {
   return (
     <Box
       color="orange" // orange to indicate TBD. remove when you start developing
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        boxShadow: 3,
+      }}
     >
-      this component takes in user prop which we can use to render a card with
-      the user info with the appropriate buttons
+      <Box>
+        <img
+          src={`https://picsum.photos/100
+          `}
+        />
+      </Box>
+      <Box sx={{ m: 1 }}>
+        <Box component="typography">Name: John Doe</Box>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Box
+            component="button"
+            color="black"
+            sx={{ bgcolor: "green", mt: 1, p: 0.5 }}
+          >
+            Accept Friend Request
+          </Box>
+          <Box component="button" sx={{ bgcolor: "red", mt: 1, ml: 1, p: 0.5 }}>
+            Decline
+          </Box>
+        </Box>
+      </Box>
     </Box>
   );
 };
--- src/pages/FriendsPage.js
+++ src/pages/FriendsPage.js
@@ -33,6 +33,7 @@ const FriendsPage = () => {
         ) : (
           <FriendsList />
         )}
+        <FriendCard />
       </Box>
     </Box>
   );
