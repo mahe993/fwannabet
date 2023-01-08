3c4174fad28475dcad44ce2df8158b4aec05a66b
Ma He <qmahe@live.com>
2023-01-08 19:24:31 +0800

    BetCard aka bet tickets are online! betting feature completed. verification feature w.i.p

--- src/constants.js
+++ src/constants.js
@@ -42,6 +42,7 @@ export const NAV_ITEMS = [
 // for menu popover
 export const POPOVER_ITEMS = [
   { name: "My Betlines", path: "/user/betlines" },
+  { name: "My Bets", path: "/user/bets" },
   { name: "Account", path: "/account" },
   { name: "Logout", path: "" },
 ];
--- src/pages/MyBetsPage.js
+++ src/pages/MyBetsPage.js
@@ -1,7 +1,82 @@
-import React from "react";
+import { Box } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import PageHeader from "../components/PageHeader";
+import CircularProgress from "@mui/material/CircularProgress";
+import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
+import { BACKEND_URL } from "../constants";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import BetCard from "../components/BetCard";
 
 const MyBetsPage = () => {
-  return <div>MyBetsPage</div>;
+  const [bets, setBets] = useState([]);
+  const [loadingData, setLoadingData] = useState(true);
+
+  const { user, getAccessTokenSilently } = useAuth0();
+
+  // get all user's bets fn
+  const getBets = async (signal) => {
+    try {
+      const accessToken = await getAccessTokenSilently();
+      const res = await axios({
+        method: "GET",
+        url: `${BACKEND_URL}/bets/${user.sub}`,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        signal,
+      });
+      setBets(res.data);
+      setLoadingData(false);
+    } catch (err) {
+      if (err.name !== "AbortError") {
+        console.log("bets data fetch aborted");
+      } else {
+        throw new Error(err);
+      }
+    }
+  };
+
+  //on mount get all user's bets
+  useEffect(() => {
+    setLoadingData(true);
+    const controller = new AbortController();
+    const signal = controller.signal;
+    getBets(signal);
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center" gap={2}>
+      <PageHeader header="My Bets" />
+      {loadingData ? (
+        <CircularProgress
+          css={css`
+            justify-self: center;
+          `}
+        />
+      ) : (
+        <Box
+          className="bets-card-container"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          gap={2}
+          width="95vw"
+        >
+          {bets.length > 0 &&
+            bets.map((bet) => (
+              <Box key={bet.id} width="100%">
+                <BetCard details={bet} />
+              </Box>
+            ))}
+        </Box>
+      )}
+    </Box>
+  );
 };
 
 export default MyBetsPage;
